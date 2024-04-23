b11017811386cfba20da5aa9a073c9dbfdbe4a6d
yaminmhd <mdyamin.drzee@gmail.com>
2024-04-23 22:01:23 +0800

    use suspense for handling loading

--- app/(products)/(routes)/products/page.tsx
+++ app/(products)/(routes)/products/page.tsx
@@ -1,11 +1,14 @@
-import React from "react";
+import React, { Suspense } from "react";
 import { Products } from "../../_components/products";
 import { ProductForm } from "../../_components/product-form";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-row justify-evenly items-center p-24">
-      <Products />
+      <Suspense fallback={<div>Loading product list...</div>}>
+        <Products />
+      </Suspense>
+
       <ProductForm />
     </main>
   );
